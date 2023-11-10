10af3ca465cd68672c43e7446b4d4150b82831e1
Rcrossmeister <403183707@QQ.COM>
2023-11-10 11:04:26 +0800

    Update test-speed.py

--- P3/test-speed.py
+++ P3/test-speed.py
@@ -24,4 +24,28 @@ torch_top5_values, torch_top5_indices = torch.topk(torch_inner_product, 5)
 end_time_torch = time.time()
 torch_time_taken = end_time_torch - start_time_torch
 
-np_time_taken, torch_time_taken, np_top5_indices, torch_top5_indices
+# 定义函数以便于重复测试
+def numpy_inner_product_topk(vector, matrix, k=5):
+    start_time = time.time()
+    inner_product = matrix.dot(vector)
+    topk_indices = np.argsort(inner_product)[-k:][::-1]
+    end_time = time.time()
+    return end_time - start_time
+
+def torch_inner_product_topk(vector, matrix, k=5):
+    start_time = time.time()
+    inner_product = torch.matmul(matrix, vector)
+    topk_values, topk_indices = torch.topk(inner_product, k)
+    end_time = time.time()
+    return end_time - start_time
+
+# 运行1000次测试并计算平均速度
+num_trials = 1000
+numpy_times = [numpy_inner_product_topk(np_vector, np_matrix) for _ in range(num_trials)]
+torch_times = [torch_inner_product_topk(torch_vector, torch_matrix) for _ in range(num_trials)]
+
+avg_numpy_time = np.mean(numpy_times)
+avg_torch_time = np.mean(torch_times)
+
+print(avg_numpy_time)
+print(avg_torch_time)
